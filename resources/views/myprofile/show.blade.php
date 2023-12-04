40a8f907ddf3f4e80ee277c3b7c8321f741bc0bc
ChawinSmch <Chawinsongmeechai@gmail.com>
2023-12-04 11:42:26 +0700

    w4

---
+++ resources/views/myprofile/show.blade.php
@@ -0,0 +1,15 @@
+<!-- this file location is /resources/views/myprofile/show.blade.php  -->
+<h1>Profile : {{ $profile->id }}</h1>
+<div>
+	<strong>Name : </strong>
+	<span>{{ $profile->name }}</span>
+</div>
+<div>
+	<strong>Last Name : </strong>
+	<span>{{ $profile->lastname }}</span>
+</div>
+<div>
+	<strong>Email : </strong>
+	<span>{{ $profile->email }}</span>
+</div>
+<div>{{ $others }}</div>
