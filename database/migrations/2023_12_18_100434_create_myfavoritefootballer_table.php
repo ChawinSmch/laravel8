896c0d6c6964c0e9b3b0594c22b012c87af4c766
ChawinSmch <Chawinsongmeechai@gmail.com>
2023-12-18 10:11:28 +0700

    w4

---
+++ database/migrations/2023_12_18_100434_create_myfavoritefootballer_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMyfavoritefootballerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('myfavoritefootballer', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->integer('Number')->nullable();
+            $table->float('Height')->nullable();
+            $table->string('Country')->nullable();
+            $table->date('Age')->nullable();
+            $table->text('Skill')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('myfavoritefootballer');
+    }
+}
